a53d4dc047ca29715d6b645859fd5648981a539d
Tine <justinereplanpogi09@gmail.com>
2026-09-22 16:50:54 +0800

    Delete earlywatch directory

--- earlywatch/api/alerts-ack-all.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once __DIR__ . "/helpers.php";
-require_method("POST");
-db()->exec("UPDATE alerts SET acknowledged = 1 WHERE acknowledged = 0");
-json_out(["ok" => true]);
--- earlywatch/api/alerts-ack.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . "/helpers.php";
-require_method("POST");
-$id = trim((string)($_GET["id"] ?? ""));
-if ($id === "") fail("Missing alert id.", 400);
-$stmt = db()->prepare("UPDATE alerts SET acknowledged = 1 WHERE id = ?");
-$stmt->execute([$id]);
-json_out(["ok" => true]);
